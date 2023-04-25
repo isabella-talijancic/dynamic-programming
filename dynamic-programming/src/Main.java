dae5f0b607b810bfdf353dd1de7c83ae8e3c47f3
Isabella <66403691+isabella-talijancic@users.noreply.github.com>
2023-04-24 20:31:05 -0500

    done!

--- dynamic-programming/src/Main.java
+++ dynamic-programming/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.HashSet;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,7 +5,6 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * Main contains all necessary classes and determines ...
@@ -18,6 +16,7 @@ import java.util.Collections;
  */
 
 public class Main {
+    
     public static void main(String[] args) {
         // Load the word list from file
         Set<String> dictionary = new HashSet<String>();
@@ -36,40 +35,33 @@ public class Main {
         String input = scanner.nextLine();
         
         // Set up dynamic programming table
-        int[] c = new int[input.length() + 1];
-        Arrays.fill(c, Integer.MAX_VALUE);
-        c[0] = 0;
+        List<List<String>> c = new ArrayList<>();
+        for (int i = 0; i < input.length() + 1; i++) {
+            c.add(new ArrayList<String>());
+        }
+        c.get(0).add("");
         
         // Fill in table bottom-up
         for (int i = 1; i <= input.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (c[j] != Integer.MAX_VALUE && dictionary.contains(input.substring(j, i))) {
-                    c[i] = Math.min(c[i], c[j] + 1);
+            for (int j = i-1; j >= 0; j--) {
+                if (!c.get(j).isEmpty() && dictionary.contains(input.substring(j, i))) {
+                    List<String> split = new ArrayList<>(c.get(j));
+                    split.add(input.substring(j, i));
+                    if (c.get(i).isEmpty() || split.size() < c.get(i).size()) {
+                        c.set(i, split);
+                    }
                 }
             }
         }
         
         // If no valid split found, output "cannot be split"
-        if (c[input.length()] == Integer.MAX_VALUE) {
+        if (c.get(input.length()).isEmpty()) {
             System.out.println(input + " cannot be split into AiW words.");
             return;
         }
         
         // Output the minimum number of splits and the resulting words
-        List<String> words = new ArrayList<>();
-        int i = input.length();
-        while (i > 0) {
-            for (int j = i-1; j >= 0; j--) {
-                if (c[j] != Integer.MAX_VALUE && dictionary.contains(input.substring(j, i))) {
-                    words.add(input.substring(j, i));
-                    i = j;
-                    break;
-                }
-            }
-        }
-        Collections.reverse(words);
-        System.out.print(input + " can be split into " + c[input.length()] + " AiW words: ");
-        System.out.println(String.join(" ", words));
-
+        System.out.print(input + " can be split into " + (c.get(input.length()).size()-1) + " AiW words: ");
+        System.out.println(String.join(" ", c.get(input.length())));
     }
 }
